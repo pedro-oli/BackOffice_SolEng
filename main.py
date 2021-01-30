9c42db2f437af1315272e8690d315aa9edc058d7
Pedro Oliveira <pedro.oli.95@outlook.com>
2021-01-30 16:58:50 -0300

    Add exception handling

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import employee
-from urllib.request import urlopen
-from xml.etree.ElementTree import parse
+import requests
+# from urllib.request import urlopen
+from requests.exceptions import HTTPError, RequestException, Timeout
+from xml.etree.ElementTree import fromstring
+#from xml.etree.ElementTree import parse
 
-var_url = urlopen("https://storage.googleapis.com/psel-2021/soleng&backoffice/Psel2021.xml")
-xmldoc = parse(var_url)
+#var_url = urlopen("https://storage.googleapis.com/psel-2021/soleng&backoffice/Psel2021.xml")
+#xmldoc = parse(var_url)
+try:
+    response = requests.get("https://storage.googleapis.com/psel-2021/soleng&backoffice/Psel2021.xml")
+    response.raise_for_status()
+except (HTTPError, RequestException, Timeout):
+    print("O download do XML falhou, por favor verifique sua conexão à internet e tente executar o programa novamente!")
+except Exception as e:
+    print("Um erro não previsto ocorreu:", e)
+else:
+    xmldoc = fromstring(response.content)
+    for item in xmldoc.iterfind("employee"):
+        name = item.findtext("name")
+        cpf = item.findtext("CPF")
+        salary = item.findtext("salary")
+        position = item.findtext("position")
+        marital_status = item.findtext("marital_status")
 
-for item in xmldoc.iterfind("employee"):
-    name = item.findtext("name")
-    cpf = item.findtext("CPF")
-    salary = item.findtext("salary")
-    position = item.findtext("position")
-    marital_status = item.findtext("marital_status")
-
-    new_employee = employee.Employee(name, cpf, salary, position, marital_status)
-    print("CPF:", cpf, "| Possíveis estados:", new_employee.possible_states())
+        new_employee = employee.Employee(name, cpf, salary, position, marital_status)
+        print("CPF:", cpf, "| Possíveis estados:", new_employee.possible_states())
